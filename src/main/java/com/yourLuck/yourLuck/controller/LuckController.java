94379a84cb4266f08901d2ec327bfcd8ae3d54cf
henryPark <selma457@gmail.com>
2024-01-23 00:06:45 +0900

    luckService

--- src/main/java/com/yourLuck/yourLuck/controller/LuckController.java
+++ src/main/java/com/yourLuck/yourLuck/controller/LuckController.java
@@ -1,17 +1,12 @@
 package com.yourLuck.yourLuck.controller;
 
-import com.yourLuck.yourLuck.controller.request.UserFortuneRequestDTO;
-import com.yourLuck.yourLuck.controller.request.UserLoginRequest;
 import com.yourLuck.yourLuck.controller.response.FortuneResponseDTO;
 import com.yourLuck.yourLuck.controller.response.Response;
-import com.yourLuck.yourLuck.controller.response.UserLoginResponse;
 import com.yourLuck.yourLuck.model.User;
-import com.yourLuck.yourLuck.model.entity.UserEntity;
 import com.yourLuck.yourLuck.service.LuckService;
 import com.yourLuck.yourLuck.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,24 +18,11 @@ public class LuckController {
     private final LuckService luckService;
 
 
-    //    @GetMapping("/todayLuck")
-//    public Response<FortuneResponseDTO> getFortune(@RequestBody UserFortuneRequestDTO request){
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        String userName = authentication.getName();
-//        User user = userService.loadUserByUserName(userName);
-//        String message = luckService.calculateFortune(user);
-//        FortuneResponseDTO responseDTO = new FortuneResponseDTO();
-//        responseDTO.setMessage(message);
-//        return Response.success(responseDTO);
-//    }}
     @GetMapping("/todayLuck")
-    public Response<FortuneResponseDTO> getFortune() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String userName = authentication.getName();
-        User user = userService.loadUserByUserName(userName);
+    public Response<FortuneResponseDTO> getFortune(@AuthenticationPrincipal User user) {
         String message = luckService.calculateFortune(user);
         FortuneResponseDTO responseDTO = new FortuneResponseDTO();
         responseDTO.setMessage(message);
         return Response.success(responseDTO);
-    };
+    }
 }
--- src/main/java/com/yourLuck/yourLuck/service/LuckService.java
+++ src/main/java/com/yourLuck/yourLuck/service/LuckService.java
@@ -12,6 +12,10 @@ import org.springframework.stereotype.Service;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -48,4 +52,54 @@ public int registrationDateToNumber(User user) {
 
         return Fortune.values()[remainder].getMessage();
     }
+
+    private int calculateTotalNumber(User user) {
+        int nameLength = user.getUsername().length();
+        int bloodTypeNumber = bloodTypeToNumber(user.getBloodType());
+        int genderNumber = genderToNumber(user.getGender());
+        int registrationNumber = registrationDateToNumber(user);
+        int birthNumber = user.getBirthOfDayAndTime().getDayOfYear();
+
+        return nameLength + bloodTypeNumber + genderNumber + registrationNumber + birthNumber;
+    }
+
+    public int[] generateLottoNumber(User user){
+        int totalNumber = calculateTotalNumber(user);
+        Random random = new Random(totalNumber);
+
+        int[] count = new int[46];
+        IntStream.range(0,100000000).forEach(i -> count[random.nextInt(45) + 1]++);
+
+        int[] result = IntStream.range(0,count.length)
+                .boxed()
+                .sorted(Comparator.comparing(i -> -count[i]))
+                .limit(6)
+                .mapToInt(i -> i)
+                .toArray();
+        Arrays.sort(result);
+        return result;
+    }
+//    public int[] generateLottoNumber(User user) {
+//        int totalNumber = calculateTotalNumber(user);
+//        Random random = new Random(totalNumber);
+//
+//        int[] count = new int[46];
+//        for(long i = 0; i < 10000000000L; i++){
+//            count[random.nextInt(45) + 1]++;
+//        }
+//        int[] result = new int[6];
+//        for(int i=0; i< 6; i++){
+//            int maxIndex = 0;
+//            for(int j = 1; j<count.length; j++){
+//                if(count[j] > count[maxIndex]){
+//                    maxIndex = j;
+//                }
+//            }
+//            result[i] = maxIndex;
+//            count[maxIndex] = -1;
+//        }
+//        Arrays.sort(result);
+//        return result;
+//    }
+
 }
